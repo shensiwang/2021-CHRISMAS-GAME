5c76fec70530cb18bddc5498ee30a958e20b1f79
shensiwang <2002597@sit.singaporetech.edu.sg>
2021-12-14 15:16:15 +0800

    test for ar test sccene

---
+++ CHRISMAS-GAME/Assets/Script/ARPlacement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacement : MonoBehaviour
+{
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+
+    private GameObject spawnedObject;
+    private Pose PlacementPose;
+    private ARRaycastManager aRRaycastManager;
+    private bool placementPoseIsValid = false;
+
+    private void Start()
+    {
+        // find all the reference here
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+    private void Update()
+    {
+        if (spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ARPlaceObject();
+        }
+        UpdatePlacementIndicator();
+        UpdatePlacementPose();
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if (spawnedObject == null && placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+    
+    void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        //set the ARObject position
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            PlacementPose = hits[0].pose;
+        }
+    }
+
+    void ARPlaceObject()
+    {
+        spawnedObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
+    }
+
+}
